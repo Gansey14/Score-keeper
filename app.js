44f57cfd2402960788cdd5629b1af9e2613e4fe6
Gansey14 <hlousek.samuel14@gmail.com>
2022-09-27 08:55:46 +0200

    first upload

---
+++ app.js
@@ -0,0 +1,12 @@
+let number1 = document.querySelector("#number1");
+let number2 = document.querySelector("#number2");
+let intnumber1 = parseInt(number1);
+
+const button1 = document.querySelector("#button1");
+const button2 = document.querySelector("#button2");
+const resetButton = document.querySelector("#resetButton");
+
+button1.addEventListener("click", function () {
+});
+
+
